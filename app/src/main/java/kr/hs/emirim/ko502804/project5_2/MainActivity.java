5bcf3cfccc22ef7439a2856bfbaac34a85fc78e9
esthe <ko990727@hmail.com>
2021-05-13 20:35:50 +0900

    Operation Event Programing commit

--- app/src/main/java/kr/hs/emirim/ko502804/project5_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project5_2/MainActivity.java
@@ -3,12 +3,21 @@ package kr.hs.emirim.ko502804.project5_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1, edit2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button[] btnNums = new Button[0];
+        for(int i =0; i<btnNums.length; i ++){
+            btnNums[i] = findViewById(i);
+        }
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
     }
 }
